5a7a57cbc0b6a7a1f81c3dbe57a4b19fa1dc5c5b
byHYONS <byhyons@gmail.com>
2024-04-23 16:04:05 +0200

    [UPD] added the logic of the exercise

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 // reset
 'use strict'
 
+// prendo elemento su HTML da trasformare e dichiaro la sua variabile
 let currentModule = document.getElementById('nome');
 console.log(currentModule);
 
 let currentText = document.getElementById('nome').innerHTML;
 console.log(currentText);
+
+// prendo gli input dell'user e li metto dentro variabili costanti
+
+
+
+// concateno il testo HTML con costanti importate dall'user
+
+
+
+// stampo in console il risultato
+
+
+
+// stampo su HTML il risultato
+
+
+
+
+// randomizzo il numero
