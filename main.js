028e679e894c3437c542695066987331eea6b182
byHYONS <byhyons@gmail.com>
2024-04-23 17:54:07 +0200

    [UPD] printed in html the prompts • Fine Esercizio

--- main.js
+++ main.js
@@ -3,26 +3,23 @@
 
 // prendo elemento su HTML da trasformare e dichiaro la sua variabile
 let currentModule = document.getElementById('nome');
-console.log(currentModule);
-
-let currentText = document.getElementById('nome').innerHTML;
-console.log(currentText);
+let currentText = currentModule.innerHTML;
 
 // prendo gli input dell'user e li metto dentro variabili costanti
+const userName = prompt('inserisci il tuo nome');
+const userSurname = prompt('inserisci il tuo cognome');
+const preferredColor = prompt('inserisci il tuo colore preferito');
 
+// BONUS • randomizzo il numero
 
 
 // concateno il testo HTML con costanti importate dall'user
-
-
-
-// stampo in console il risultato
-
-
+currentText = `${currentText} ${userName} ${userSurname} e il mio colore preferito è il ${preferredColor}`;
 
 // stampo su HTML il risultato
+currentModule.innerHTML = currentText
+
+// BONUS •  stampo in console il risultato
 
 
 
-
-// randomizzo il numero
